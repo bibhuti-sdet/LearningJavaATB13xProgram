0e46fd6992b20342875ded74b9f2a0ff2caf7f83
bibhu <bibhutirpa12@gmail.com>
2025-06-05 09:39:17 +0530

    2nd commit

--- ex_01_Java_Basic/Lab001_HellowWorld.java
+++ ex_01_Java_Basic/Lab001_HellowWorld.java
@@ -2,7 +2,7 @@ package ex_01_Java_Basic;
 
 public class Lab001_HellowWorld {
     public static void main(String[] args) {
-        System.out.println("2nd hellow program");
+        System.out.println("hello program");
     }
 
     }
---
+++ ex_01_Java_Basic/Lab002_Datatypes.java
@@ -0,0 +1,31 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Datatypes {
+    public static void main(String[] args) {
+        // Integer types
+        byte byteVar = 100;
+        short shortVar = 10000;
+        int intVar = 1000000;
+        long longVar = 10000000000L;
+
+        // Floating-point types
+        float floatVar = 3.14f;
+        double doubleVar = 3.14159265359;
+
+        // Character type
+        char charVar = 'A';
+
+        // Boolean type
+        boolean booleanVar = true;
+
+        // Printing the values
+        System.out.println("Byte: " + byteVar);
+        System.out.println("Short: " + shortVar);
+        System.out.println("Integer: " + intVar);
+        System.out.println("Long: " + longVar);
+        System.out.println("Float: " + floatVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Character: " + charVar);
+        System.out.println("Boolean: " + booleanVar);
+    }
+}
