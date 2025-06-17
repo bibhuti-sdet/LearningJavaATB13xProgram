e7f3bf30b343df5c6a281a9164e1f9c137bdbdc4
bibhu <bibhutirpa12@gmail.com>
2025-06-17 23:15:22 +0530

    06/17/2025

---
+++ ex_01_Java_Basic/Lab004_shortcutHW.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab004_shortcutHW {
+    public static void main(String[] args) {
+    //type main , then press ctrl+space enter
+    }
+}
---
+++ ex_01_Java_Basic/Lab005_NoMainMethod.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab005_NoMainMethod {
+    public void calc() {
+        int i  = 2;
+        int j = 5;
+
+
+        }
+    }
+
+
