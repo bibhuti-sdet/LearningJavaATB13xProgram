b5ceb3431958bbcaa1377fbd6d9ec1bded6b79c6
bibhu <bibhutirpa12@gmail.com>
2025-06-25 06:53:30 +0530

    06/09/2025

---
+++ ex_02_Java_Basics/Lab015_Variables.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basics;
+
+public class Lab015_Variables {
+    public static void main(String[] args) {
+        // datatype Variablename = variable value
+        byte b = 10;
+
+    }
+}
