39b29fe9e32a45fa0be1321b846ef7e90969134b
bibhu <bibhutirpa12@gmail.com>
2025-09-14 00:22:51 +0530

    09/14/2025

---
+++ ex_31_Literals/Lab091_Final_Example.java
@@ -0,0 +1,14 @@
+package ex_31_Literals;
+
+public class Lab091_Final_Example {
+    public static class Lab028_Final_Example {
+        public static void main(String[] args) {
+            int local = 0;
+            System.out.println(local);
+
+            final float PI = 3.14f;
+            // PI = 3.13f; This is final in nature.
+            System.out.println(PI);
+        }
+    }
+}
